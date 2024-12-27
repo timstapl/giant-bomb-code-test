c60978bc65be4b7d7697de195970a45c7f3c2fa1
Tim Stapleton <tim.stapleton@hey.com>
2024-12-27 10:19:22 -0600

    Tweak search page

--- pages/search.vue
+++ pages/search.vue
@@ -2,13 +2,13 @@
   <div class="bg-shack-gray h-screen w-full flex flex-col items-center px-12">
     <div id="spacer" class="h-32"></div>
     <div class="text-shack-text w-full flex flex-row justify-center items-center">
-      <div class="w-4/5 flex">
-        <label class="w-1/3 text-right pr-4" for="search">Search</label>
+      <div class="w-full flex justify-center items-center">
+        <label class="w-1/3 text-right pr-4 sr-only" for="search">Find your next Adventure</label>
         <input class="w-2/5 bg-shack-text rounded-lg text-shack-gray" type="text" id="search" v-model="query"></input>
       </div>
     </div>
     <div class="text-shack-text w-full flex flex-row justify-center items-center mt-4">
-        <button @click="search">Submit</button>
+        <button class="bg-shack-orange p-4 rounded-lg text-shack-gray font-bold" @click="search">Find your next Adventure</button>
     </div>
   </div>
 </template>
