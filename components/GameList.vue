482337d1e6a04a5f9fee2f891a7c16b11e472a46
Tim Stapleton <tim.stapleton@hey.com>
2024-12-27 15:09:57 -0600

    update game list to use nuxtlinks to avoid hard reloads

--- components/GameList.vue
+++ components/GameList.vue
@@ -4,11 +4,11 @@
       <h2 class="sr-only">Search Results</h2>
 
       <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        <a v-for="game in gamesStore.results" :key="game.id" :href="game.site_detail_url" class="group">
+        <NuxtLink v-for="game in gamesStore.results" :key="game.id" :href="`/games/${game.guid}`" class="group">
           <img :src="game.image.thumb_url" :alt="`${game.name}-thumbnail`" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]" />
           <h3 class="mt-4 text-sm text-shack-text">{{ game.name }}</h3>
           <!-- <p class="mt-1 text-lg font-medium text-shack-text">{{ product.price }}</p> -->
-        </a>
+        </NuxtLink>
       </div>
     </div>
   </div>
